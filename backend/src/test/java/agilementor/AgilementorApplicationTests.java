6a53c0c1c1d8464fdabfcb1df01a462da632ff27
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-10-10 03:06:02 +0900

    feat: 백엔드 프로젝트 초기 세팅

---
+++ backend/src/test/java/agilementor/AgilementorApplicationTests.java
@@ -0,0 +1,13 @@
+package agilementor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilementorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
