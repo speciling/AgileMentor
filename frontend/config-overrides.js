9c212d217527e98f7819d3027812daec2c420bff
dlawlghks <ikuwlghks@naver.com>
2024-11-05 13:40:59 +0900

    feat: 경로 별칭(alias) 설정 추가
    - jsconfig.json과 tsconfig.paths.json에 경로 별칭 설정을 추가하여 가독성 향상
    - Webpack의 config-overrides.js에 @components, @features, @styles, @pages 별칭 설정 추가
    - 프로젝트 전반에서 더 깔끔하고 유지 보수하기 쉬운 import 구조 적용

---
+++ frontend/config-overrides.js
@@ -0,0 +1,13 @@
+// config-overrides.js
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { override, addWebpackAlias } = require('customize-cra');
+
+module.exports = override(
+  addWebpackAlias({
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@features': path.resolve(__dirname, 'src/components/features'),
+    '@styles': path.resolve(__dirname, 'src/styles'),
+    '@pages': path.resolve(__dirname, 'src/pages'),
+  }),
+);
