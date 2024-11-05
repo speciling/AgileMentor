a398870774360feeacea1c92f2c86fa2c752a331
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-11-05 13:48:53 +0900

    feat: idToken 발급 요청 기능 추가

--- backend/src/main/java/agilementor/member/service/AuthService.java
+++ backend/src/main/java/agilementor/member/service/AuthService.java
@@ -3,17 +3,21 @@ package agilementor.member.service;
 import agilementor.member.properties.GoogleClientProperties;
 import agilementor.member.properties.GoogleProviderProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Service
 public class AuthService {
 
     private final GoogleClientProperties clientProperties;
     private final GoogleProviderProperties providerProperties;
+    private final GoogleOidcClient oidcClient;
 
     public AuthService(GoogleClientProperties clientProperties,
-        GoogleProviderProperties providerProperties) {
+        GoogleProviderProperties providerProperties, GoogleOidcClient oidcClient) {
         this.clientProperties = clientProperties;
         this.providerProperties = providerProperties;
+        this.oidcClient = oidcClient;
     }
 
     public String getAuthUrl() {
@@ -24,4 +28,17 @@ public class AuthService {
             + "&scope=" + String.join(" ", clientProperties.scope())
             + "&redirect_uri=" + clientProperties.redirectUri();
     }
+
+    public String requestIdToken(String code) {
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", code);
+        body.add("client_id", clientProperties.clientId());
+        body.add("client_secret", clientProperties.clientSecret());
+        body.add("redirect_uri", clientProperties.redirectUri());
+        body.add("grant_type", "authorization_code");
+
+        return oidcClient.callTokenApi(body);
+    }
+
 }
---
+++ backend/src/main/java/agilementor/member/service/GoogleOidcClient.java
@@ -0,0 +1,39 @@
+package agilementor.member.service;
+
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
+
+import agilementor.member.dto.GoogleTokenResponse;
+import agilementor.member.properties.GoogleProviderProperties;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class GoogleOidcClient {
+
+    private final GoogleProviderProperties providerProperties;
+    private final RestClient restClient;
+
+    public GoogleOidcClient(GoogleProviderProperties providerProperties) {
+        this.providerProperties = providerProperties;
+        restClient = RestClient.builder().build();
+    }
+
+    public String callTokenApi(MultiValueMap<String, String> body) {
+        return restClient.post()
+            .uri(providerProperties.tokenUri())
+            .contentType(APPLICATION_FORM_URLENCODED)
+            .body(body)
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                throw new IllegalArgumentException("카카오 토큰 발급 실패");
+            })
+            .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                throw new RuntimeException("카카오 인증 서버 에러");
+            })
+            .toEntity(GoogleTokenResponse.class)
+            .getBody()
+            .idToken();
+    }
+}
