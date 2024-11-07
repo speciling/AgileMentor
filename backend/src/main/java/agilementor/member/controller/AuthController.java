7a8146eaf08f065055deff606b2355b38c7ed650
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-11-07 09:42:21 +0900

    refactor: AuthService 이름 변경, OidcClient와 통합

--- backend/src/main/java/agilementor/member/controller/AuthController.java
+++ backend/src/main/java/agilementor/member/controller/AuthController.java
@@ -1,6 +1,6 @@
 package agilementor.member.controller;
 
-import agilementor.member.service.AuthService;
+import agilementor.member.service.AuthClientService;
 import agilementor.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthClientService authClientService;
     private final MemberService memberService;
 
-    public AuthController(AuthService authService, MemberService memberService) {
-        this.authService = authService;
+    public AuthController(AuthClientService authClientService, MemberService memberService) {
+        this.authClientService = authClientService;
         this.memberService = memberService;
     }
 
     @GetMapping("/login")
     public String redirectGoogleLogin() {
-        return "redirect:" + authService.getAuthUrl();
+        return "redirect:" + authClientService.getAuthUrl();
     }
 
     @GetMapping("/login/code/google")
@@ -31,7 +31,7 @@ public class AuthController {
         String idToken;
 
         try {
-            idToken = authService.requestIdToken(code);
+            idToken = authClientService.requestIdToken(code);
         } catch (Exception e) {
             // todo: 로그인 실패 페이지 추가
             return "redirect:/";
--- backend/src/main/java/agilementor/member/service/AuthClientService.java
+++ backend/src/main/java/agilementor/member/service/AuthClientService.java
@@ -3,24 +3,46 @@ package agilementor.member.service;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
 
 import agilementor.member.dto.GoogleTokenResponse;
+import agilementor.member.properties.GoogleClientProperties;
 import agilementor.member.properties.GoogleProviderProperties;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
 @Service
-public class GoogleOidcClient {
+public class AuthClientService {
 
+    private final GoogleClientProperties clientProperties;
     private final GoogleProviderProperties providerProperties;
     private final RestClient restClient;
 
-    public GoogleOidcClient(GoogleProviderProperties providerProperties) {
+    public AuthClientService(GoogleClientProperties clientProperties,
+        GoogleProviderProperties providerProperties) {
+        this.clientProperties = clientProperties;
         this.providerProperties = providerProperties;
         restClient = RestClient.builder().build();
     }
 
-    public String callTokenApi(MultiValueMap<String, String> body) {
+    public String getAuthUrl() {
+        return providerProperties.authorizationUri()
+            + "?"
+            + "response_type=code"
+            + "&client_id=" + clientProperties.clientId()
+            + "&scope=" + String.join(" ", clientProperties.scope())
+            + "&redirect_uri=" + clientProperties.redirectUri();
+    }
+
+    public String requestIdToken(String code) {
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", code);
+        body.add("client_id", clientProperties.clientId());
+        body.add("client_secret", clientProperties.clientSecret());
+        body.add("redirect_uri", clientProperties.redirectUri());
+        body.add("grant_type", "authorization_code");
+
         return restClient.post()
             .uri(providerProperties.tokenUri())
             .contentType(APPLICATION_FORM_URLENCODED)
@@ -36,4 +58,5 @@ public class GoogleOidcClient {
             .getBody()
             .idToken();
     }
+
 }
