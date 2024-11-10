aa105c8c1a84c616169b2cd9b9a9a0d01f727d7b
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-11-10 12:01:13 +0900

    feat: 프로젝트, 프로젝트 회원 entity 생성

---
+++ backend/src/main/java/agilementor/project/entity/Project.java
@@ -0,0 +1,19 @@
+package agilementor.project.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projectId;
+
+    @Column(nullable = false)
+    private String title;
+
+}
---
+++ backend/src/main/java/agilementor/project/entity/ProjectMember.java
@@ -0,0 +1,27 @@
+package agilementor.project.entity;
+
+import agilementor.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ProjectMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projectMemberId;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
+
+
