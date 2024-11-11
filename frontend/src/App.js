23e578ca536d6136bdd02e8435c382a15ac66cde
dlawlghks <ikuwlghks@naver.com>
2024-11-11 23:12:17 +0900

    feat: 충돌 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 // eslint-disable-next-line import/no-unresolved
 import Header from '@components/features/Header';
 // eslint-disable-next-line import/no-unresolved
-import DashboardPage from '@pages/Dashboard';
-// eslint-disable-next-line import/no-unresolved
+import SideBar from '@components/features/SideBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <DashboardPage />
+      <SideBar />
     </div>
   );
 }
