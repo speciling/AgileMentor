14201d29b0b44d3e886fd7020978782e5bd3327d
Tam Oh <tamhappy18@gmail.com>
2024-11-12 05:28:19 +0900

    feat: mockSprints 추가

---
+++ frontend/src/mocks/mockSprints.js
@@ -0,0 +1,12 @@
+const mockSprints = [
+    { sprint_id: 201, end_date: '2024-11-15', completed_tasks_count: 7 },
+    { sprint_id: 202, end_date: '2024-11-22', completed_tasks_count: 5 },
+    { sprint_id: 203, end_date: '2024-11-29', completed_tasks_count: 6 },
+    { sprint_id: 204, end_date: '2024-12-06', completed_tasks_count: 8 },
+    { sprint_id: 205, end_date: '2024-12-13', completed_tasks_count: 5 },
+    { sprint_id: 206, end_date: '2024-12-20', completed_tasks_count: 7 },
+    { sprint_id: 207, end_date: '2024-12-27', completed_tasks_count: 4 },
+    { sprint_id: 208, end_date: '2025-01-03', completed_tasks_count: 8 },
+  ];
+  
+  export default mockSprints;
