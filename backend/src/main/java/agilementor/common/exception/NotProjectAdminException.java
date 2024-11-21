e82d9ee4efdc7fa3fa8eb047f32583797217f033
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-11-21 19:28:15 +0900

    fix: 프로젝트 관리자가 아닐 때 오류 메세지 수정

--- backend/src/main/java/agilementor/common/exception/NotProjectAdminException.java
+++ backend/src/main/java/agilementor/common/exception/NotProjectAdminException.java
@@ -2,7 +2,7 @@ package agilementor.common.exception;
 
 public class NotProjectAdminException extends RuntimeException {
 
-    private static final String MESSAGE = "해당 회원이 존재하지 않습니다.";
+    private static final String MESSAGE = "프로젝트 관리자가 아닙니다.";
 
     public NotProjectAdminException() {
         super(MESSAGE);
