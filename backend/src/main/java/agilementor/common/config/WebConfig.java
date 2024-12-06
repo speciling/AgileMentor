b8995bb0a0e8ad46a8d0e66b9dd1de16c7f52b1b
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-12-06 16:18:30 +0900

    feat: cors 설정 추가

--- backend/src/main/java/agilementor/common/config/WebConfig.java
+++ backend/src/main/java/agilementor/common/config/WebConfig.java
@@ -11,6 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOriginPatterns("*")  // todo: 테스트가 끝나면 https://agilementor.kr로 수정
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .exposedHeaders("Location")
+            .maxAge(1800)
+            .allowCredentials(true);
+    }
+
     @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
